64d13e4b6f2176767f7e91de1eef46cafb091943
Graham Thompson <grahamwt42@gmail.com>
2018-01-15 17:39:25 -0800

    adds plot of cost function

--- assignment1-linearRegression/gradient_descent.py
+++ assignment1-linearRegression/gradient_descent.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 def computeCost(X, y, theta):
 	mo = (X * theta)
@@ -18,7 +19,7 @@ def gradientDescent(X, y, theta, alpha, num_iters):
 		t1 = theta[1] - ( alpha * np.sum( (htheta - y)*X[:,1] ) / m )
 		theta[0] = t0
 		theta[1] = t1
-		print( computeCost(X,y,theta) )
+		#print( computeCost(X,y,theta) )
 		
 		
 def plotFit(x, y, theta):
@@ -26,10 +27,30 @@ def plotFit(x, y, theta):
 	ax = fig.add_subplot(1,1,1)
 	ax.scatter(x, y)
 	ax.plot(x, theta[1]*x + theta[0], '-', color='r')
+	ax.set_title('Food Truck Profits',fontsize=10)
 	ax.set_xlabel('Population of City in 10,000s',fontsize=10)
 	ax.set_ylabel('Profit in $10,000s',fontsize=10)
 	plt.show()
 	
+	
+def plotCost(X, y, theta):
+	theta0 = np.linspace(-10, 10, 100);
+	theta1 = np.linspace(-1, 4, 100);
+	j_vals = np.zeros( (len(theta0), len(theta1)) )
+	
+	for i in range(len(theta0)):
+		for j in range(len(theta1)):
+			j_vals[i,j] = computeCost(X, y, np.array([theta0[i], theta1[j]]) )
+			
+	xval, yval = np.meshgrid(theta0, theta1)
+	fig = plt.figure()
+	ax = fig.gca(projection='3d')
+	surf = ax.plot_surface(xval, yval, j_vals)
+	ax.set_title('Cost Function',fontsize=10)
+	ax.set_xlabel('Theta0',fontsize=10)
+	ax.set_ylabel('Theta1',fontsize=10)
+	plt.show()
+
 	
 if __name__ == "__main__":
 	tmp = []
@@ -48,3 +69,4 @@ if __name__ == "__main__":
 
 	gradientDescent(ft_data[:,0:2], ft_data[:,2], theta, alpha, 1000)
 	plotFit(ft_data[:,1], ft_data[:,2], theta)
+	plotCost(ft_data[:,0:2], ft_data[:,2], theta)
